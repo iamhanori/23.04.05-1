b4c71554463158662bb8143f24b1d1899c68f47b
Hanul Kim <s2122@e-mirim.hs.kr>
2023-04-05 12:22:01 +0900

    @value로 값 주입하면 set이 필요없다

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -11,8 +11,8 @@ public class Main {
         TestBean1 obj1 = ctx.getBean(TestBean1.class);
 
         TestBean2 obj2 = ctx.getBean(TestBean2.class);
-        obj2.setData1(11);
-        obj2.setData2(11.11);
+      //  obj2.setData1(11);
+      //  obj2.setData2(11.11);
         System.out.println("obj2.getData1 : " + obj2.getData1());
         System.out.println("obj2.getData2 : " + obj2.getData2());
 
