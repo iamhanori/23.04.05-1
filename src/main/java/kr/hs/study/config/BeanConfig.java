ce648676038e4eeaa9c6bda9629b98ace743c825
Hanul Kim <s2122@e-mirim.hs.kr>
2023-04-05 12:12:20 +0900

    Annotation

---
+++ src/main/java/kr/hs/study/config/BeanConfig.java
@@ -0,0 +1,10 @@
+package kr.hs.study.config;
+
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// @ComponentScans(basePackages = "kr.hs.study.beans")
+public class BeanConfig {
+}
+
