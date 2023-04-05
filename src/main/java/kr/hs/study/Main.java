ab029c1ac8b2be71336191cf638fa3b547d5abbc
Hanul Kim <s2122@e-mirim.hs.kr>
2023-04-05 12:21:03 +0900

    getter setter로 값 주입

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,6 +1,7 @@
 package kr.hs.study;
 
 import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
@@ -9,6 +10,16 @@ public class Main {
 
         TestBean1 obj1 = ctx.getBean(TestBean1.class);
 
+        TestBean2 obj2 = ctx.getBean(TestBean2.class);
+        obj2.setData1(11);
+        obj2.setData2(11.11);
+        System.out.println("obj2.getData1 : " + obj2.getData1());
+        System.out.println("obj2.getData2 : " + obj2.getData2());
+
         ctx.close();
+
+
+
+
     }
 }
